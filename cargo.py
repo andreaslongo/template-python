e03821d8ff906462e470d7412a6f454da946d4ce
Andreas Longo <git@andreaslongo.com>
2023-07-21 21:06:28 +0200

    Black formatting without --preview

--- cargo.py
+++ cargo.py
@@ -1,23 +1,38 @@
 #!/usr/bin/env python3
 
+
 def main():
     import argparse
     import subprocess
 
     parser = argparse.ArgumentParser()
     commands = parser.add_subparsers(dest="command", title="commands")
-    commands.add_parser("clippy", help="Checks a package to catch common mistakes and improve your Python code.")
+    commands.add_parser(
+        "clippy",
+        help="Checks a package to catch common mistakes and improve your Python code.",
+    )
     commands.add_parser("dev", help="Initialize a local DEV environment.")
-    commands.add_parser("fmt", help="Formats all Python files of the local package using black.")
+    commands.add_parser(
+        "fmt", help="Formats all Python files of the local package using black."
+    )
     commands.add_parser("nox", help="Execute all nox sessions.")
-    commands.add_parser("test", help="Execute all unit and integration tests and build examples of a local package")
+    commands.add_parser(
+        "test",
+        help="Execute all unit and integration tests and build examples of a local package",
+    )
     args = parser.parse_args()
 
-    if args.command == "clippy": subprocess.run((".nox/dev/bin/ruff", "check", "."))
-    if args.command == "dev": subprocess.run(("python3", "-m", "pipx", "run", "nox", "--session", "dev"))
-    if args.command == "fmt": subprocess.run((".nox/dev/bin/black", "--quiet", "--preview", "."))
-    if args.command == "nox": subprocess.run(("python3", "-m", "pipx", "run", "nox"))
-    if args.command == "test": subprocess.run((".nox/dev/bin/pytest"))
+    if args.command == "clippy":
+        subprocess.run((".nox/dev/bin/ruff", "check", "."))
+    if args.command == "dev":
+        subprocess.run(("python3", "-m", "pipx", "run", "nox", "--session", "dev"))
+    if args.command == "fmt":
+        subprocess.run((".nox/dev/bin/black", "--quiet", "."))
+    if args.command == "nox":
+        subprocess.run(("python3", "-m", "pipx", "run", "nox"))
+    if args.command == "test":
+        subprocess.run((".nox/dev/bin/pytest"))
+
 
 if __name__ == "__main__":
     main()
