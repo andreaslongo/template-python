d96491682e74e35f4ad430e3afda2b3e9f6097f5
Andreas Longo <git@andreaslongo.com>
2024-08-13 13:38:24 +0200

    Run pip check after install

--- noxfile.py
+++ noxfile.py
@@ -6,12 +6,14 @@ nox.options.sessions = ["test", "lint"]
 @nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12"])
 def test(session):
     session.install(".[test]")
+    session.run("pip", "check")
     session.run("pytest")
 
 
 @nox.session
 def lint(session):
     session.install(".[lint]")
+    session.run("pip", "check")
     session.run("ruff", "check", ".")
     session.run("ruff", "format", "--diff", "--quiet", ".")
 
@@ -19,3 +21,4 @@ def lint(session):
 @nox.session
 def dev(session):
     session.install("--editable", ".[test, lint]")
+    session.run("pip", "check")
