79edfda2c84b664d461cc1e7a53d77c09b170097
Andreas Longo <git@andreaslongo.com>
2024-08-13 14:41:12 +0200

    Update

--- src/template_python/cli.py
+++ src/template_python/cli.py
@@ -4,6 +4,6 @@ from template_python.lib import say_hello
 
 def run():
     parser = argparse.ArgumentParser()
-    args = parser.parse_args()
+    _args = parser.parse_args()
 
     print(say_hello())
