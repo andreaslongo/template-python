6837a072e324b669898fdecf5f0e6e41a09870f4
Andreas Longo <git@andreaslongo.com>
2024-08-24 11:05:48 +0200

    Switch to pixi package manager

--- container/build_prod.sh
+++ container/build_prod.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Builds a new DEV container image
+# Builds a new PROD container image
 #
 # TODO: Update BASE_IMAGE when a new Debian stable gets released.
 #
@@ -8,7 +8,6 @@
 # * https://github.com/debuerreotype/docker-debian-artifacts
 # * https://github.com/docker-library/official-images
 
-readonly local base_image=docker.io/library/debian:bookworm-slim
 readonly local script_dir=$( cd "$( dirname "${BASH_SOURCE[0]:-${(%):-%x}}" )" && pwd )
 readonly local parent_dir=$(dirname ${script_dir})
 
@@ -19,12 +18,16 @@ readonly local parent_dir=$(dirname ${script_dir})
 # Use --pull=always to do this also for images without `latest` tags.
 # NOTE: Named volumes not supported during build, only bind-mounts are supported.
 podman image build \
-    --build-arg=BASE_IMAGE="${base_image}" \
+    --build-arg=BASE_IMAGE=docker.io/library/debian:bookworm-slim \
     --file="${script_dir}/Containerfile" \
-    --ignorefile="${script_dir}/Containerignore" \
+    --ignorefile="${script_dir}/Containerignore_prod" \
     --no-cache \
     --pull=always \
-    --tag="$(basename ${parent_dir})":"$(date --iso-8601)" \
-    --tag="$(basename ${parent_dir})":latest \
-    --target=dev \
+    --tag="$(basename ${parent_dir})_prod":"$(date --iso-8601)" \
+    --tag="$(basename ${parent_dir})_prod":latest \
+    --target=prod \
+    --volume "${HOME}/.cache/huggingface:/root/.cache/huggingface:z,rw" \
+    --volume "${HOME}/.cache/pre-commit:/root/.cache/pre-commit:z,rw" \
+    --volume "${HOME}/.cache/rattler:/root/.cache/rattler:z,rw" \
+    --volume "${HOME}/.pixi:/root/.pixi:z,rw" \
         "${parent_dir}"
--- container/run.sh
+++ container/run.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+# Starts a new container based on the DEV image or attaches to an already
+# running DEV container.
+#
+# Programs run as non-root-user inside the container for security reasons.
+#
+# The container mounts the project directory via bind-mount. Only a single
+# container (and the host) can access the project files (`:Z` uppercase).
+#
+# The container uses named volumes for persisting cache files to speed up the
+# development process. Multiple containers can access the cache volumes
+# simultaneously (`:z` lowercase).
+
 readonly local script_dir=$( cd "$( dirname "${BASH_SOURCE[0]:-${(%):-%x}}" )" && pwd )
 readonly local parent_dir=$(dirname ${script_dir})
 
@@ -14,9 +26,14 @@ podman container start --attach --interactive "$(basename ${parent_dir})" 2>/dev
     --interactive \
     --name="$(basename ${parent_dir})" \
     --pull=newer \
+    --rm \
     --tty \
     --user ${uid}:${gid} \
     --userns keep-id:uid=${uid},gid=${gid} \
+    --volume "${HOME}/.cache/huggingface:/home/appuser/.cache/huggingface:z,rw" \
+    --volume "${HOME}/.cache/pre-commit:/home/appuser/.cache/pre-commit:z,rw" \
+    --volume "${HOME}/.cache/rattler:/home/appuser/.cache/rattler:z,rw" \
+    --volume "${HOME}/.pixi:/home/appuser/.pixi:z,rw" \
     --volume "${parent_dir}:/home/appuser/$(basename ${parent_dir}):Z,rw" \
     --workdir "/home/appuser/$(basename ${parent_dir})" \
         localhost/"$(basename ${parent_dir})":latest
